d43265bcf753719cd25784b6f43677209ea3c840
meshu-dev <harmeshuppal@gmail.com>
2023-04-09 17:06:21 +0100

    Tidy up code

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
-import click
 from config import get_config
 from holidays import import_holidays
 from db.holidays import get_all_bank_holidays, get_next_bank_holiday, get_bank_holiday_by_name_and_date
+from utils import get_day_suffix
 
 app = Flask(__name__, template_folder='templates')
 app.config.from_object('config')
@@ -11,11 +11,7 @@ app.config.from_object('config')
 def index():
     next_holiday = get_next_bank_holiday()
     day = next_holiday.date.day
-
-    if 4 <= day <= 20 or 24 <= day <= 30:
-        suffix = "th"
-    else:
-        suffix = ["st", "nd", "rd"][day % 10 - 1]
+    suffix = get_day_suffix(day)
 
     return render_template(
         'index.html',
@@ -24,20 +20,9 @@ def index():
         suffix=suffix
     )
 
-@app.route('/test')
-def test():
-    holidays = get_all_bank_holidays()
-
-    for holiday in holidays:
-        print('DB Holiday', holiday.name, holiday.date)
-
-    return render_template('index.html')
-
 @app.cli.command('import-data')
 def import_data():
     import_holidays()
-
-print('name', __name__)
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8000)
--- app/utils.py
+++ app/utils.py
@@ -13,3 +13,11 @@ def get_content_from_url(url):
 def get_file_path(file):
     basedir = path.abspath(path.dirname(__file__))
     return path.join(basedir, file)
+
+def get_day_suffix(day):
+    if 4 <= day <= 20 or 24 <= day <= 30:
+        suffix = "th"
+    else:
+        suffix = ["st", "nd", "rd"][day % 10 - 1]
+    
+    return suffix
